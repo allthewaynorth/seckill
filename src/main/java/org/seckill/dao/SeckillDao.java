7a1dce271734c3021fabbee68485f2e296f285a7
allthewaynorth <atwnorth@163.com>
2016-06-03 20:29:58 +0800

    1

---
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -0,0 +1,23 @@
+package org.seckill.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.seckill.entity.Seckill;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by lenovo on 2016/6/2.
+ */
+public interface SeckillDao {
+    //减库存,如果影响行数>1,表示更新的记录行数
+    int reduceNumber(@Param("seckillId") long seckillId, @Param("killTime") Date killTime);
+
+    //根据id查询秒杀对象
+    Seckill queryById(long seckillId);
+
+    //根据偏移量查询秒杀商品列表
+    List<Seckill> queryAll(@Param("offset") int offset, @Param("limit") int limit);
+
+
+}
---
+++ src/main/java/org/seckill/dao/SuccessKilledDao.java
@@ -0,0 +1,15 @@
+package org.seckill.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.seckill.entity.SuccessKilled;
+
+/**
+ * Created by lenovo on 2016/6/2.
+ */
+public interface SuccessKilledDao {
+    //插入购买明细,可过滤重复,插入的行数
+    int insertSuccessKilled(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
+
+    //根据id查询SuccessKilled并携带秒杀产品对象实体
+    SuccessKilled queryByIdWithSeckill(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
+}
